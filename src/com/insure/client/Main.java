ddd02b8b00ab32392eff32cd6ef652de21ebfcdc
AntonioAlem <Molengo1>
2020-02-03 10:49:58 +0000

    teste git client

--- src/com/insure/client/Main.java
+++ src/com/insure/client/Main.java
@@ -3,5 +3,6 @@ package com.insure.client;
 public class Main {
     public static void main(String args[]){
         System.out.println("Project template - client");
+        ///odjssvkbsakvabsv
     }
 }
