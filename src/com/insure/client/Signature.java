31b90421362880b28ef49aaba69179ba5164252a
AntonioAlem <Molengo1>
2020-02-07 14:40:37 +0000

    update

--- src/com/insure/client/Signature.java
+++ src/com/insure/client/Signature.java
@@ -12,57 +12,57 @@ import java.util.Base64;
 
 public class Signature {
 
-        private String msg;
-        private String hash;
-        private String encryptedHash;
-        private PrivateKey privatekey;
-        private PublicKey publickey;
+    private String msg;
+    private String hash;
+    private String encryptedHash;
+    private PrivateKey privatekey;
+    private PublicKey publickey;
 
-    public Signature()  {
+    public Signature() {
     }
 
     //Cria o Hash da mensagem a enviar
     private String createHash(String msg) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        MessageDigest digest=MessageDigest.getInstance("SHA-256");
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
         return Base64.getEncoder().encodeToString(digest.digest(msg.getBytes("UTF-8")));
     }
 
     //Encripta a mensagem
-    private String encryptedHash(String hash,String filename) throws Exception {
-        EncryptPrivate PrivateEncrypt= new EncryptPrivate();
-        this.privatekey=PrivateEncrypt.getPrivate(filename);
-        return PrivateEncrypt.encryptText (hash,privatekey);
+    private String encryptedHash(String hash, String filename) throws Exception {
+        EncryptPrivate PrivateEncrypt = new EncryptPrivate();
+        this.privatekey = PrivateEncrypt.getPrivate(filename);
+        return PrivateEncrypt.encryptText(hash, privatekey);
     }
 
     //Desincripta a mensagem
     private String decryptHash(String fileName, String encryptedHash) throws Exception {
-        DecryptPublic privateDecrypt= new DecryptPublic();
-        publickey=privateDecrypt.getPublic(fileName);
-        return privateDecrypt.decryptText(encryptedHash,publickey);
+        DecryptPublic privateDecrypt = new DecryptPublic();
+        publickey = privateDecrypt.getPublic(fileName);
+        return privateDecrypt.decryptText(encryptedHash, publickey);
     }
 
     public String createSignature(String fileName, String content) throws Exception {
         String hash = createHash(content);
-        return encryptedHash(hash,fileName);
+        return encryptedHash(hash, fileName);
     }
 
     //verificaçao da assinatura:
-    public boolean verifySignature(String content,String encryptedHash,String filename) throws Exception {
-        String decryptedHash =this.decryptHash(filename,encryptedHash);
+    public boolean verifySignature(String content, String encryptedHash, String filename) throws Exception {
+        String decryptedHash = this.decryptHash(filename, encryptedHash);
         String Hash = createHash(content);
-        if (hash.equals(decryptedHash)){
+        if (hash.equals(decryptedHash)) {
             return true;
-        }else{
+        } else {
             throw new UserException_Exception("You are not allowed to add document do this claim. Please try again");
         }
     }
 
-        //public static void main(String[] args) throws Exception {
-            //Signature signature=new Signature("c","keys\\user1\\user1PrivateKey");
-            //System.out.println(signature.getMsg()+"\n");
-            //System.out.println(signature.getHash()+"\n");
-           // System.out.println(signature.getEncryptedHash()+"\n");
-        //}
+    //public static void main(String[] args) throws Exception {
+    //Signature signature=new Signature("c","keys\\user1\\user1PrivateKey");
+    //System.out.println(signature.getMsg()+"\n");
+    //System.out.println(signature.getHash()+"\n");
+    // System.out.println(signature.getEncryptedHash()+"\n");
+    //}
 }
 
 
